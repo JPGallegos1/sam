9f9c60501df4a78082cbb72a765b0e4a1210e4af
Juan Pablo <jpgallegos20@gmail.com>
2023-12-08 04:00:04 -0300

    add: FAQ section

--- src/features/landing/header.tsx
+++ src/features/landing/header.tsx
@@ -52,8 +52,8 @@ export default function Header() {
                 </ul>
             </nav>
             <section className="py-28">
-                <div className="max-w-screen-xl mx-auto text-gray-600 gap-x-12 items-center justify-between overflow-hidden md:flex md:px-8">
-                    <div className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl">
+                <div className="max-w-screen-xl mx-auto text-gray-600 gap-x-12 items-center justify-between overflow-hidden md:flex ">
+                    <div className="flex-none space-y-5 sm:max-w-lg md:px-0 lg:max-w-xl">
                         <h1 className="text-sm text-sky-600 font-medium">
                         {`Facilitating over 1200 academic achievements.`}
                         </h1>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@/features/landing/header'
+import FAQ from '@/features/landing/FAQ'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <Header />
+      <FAQ />
     </>
   )
 }
